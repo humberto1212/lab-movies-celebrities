86cc9b7b29e178d4aaf3222bf12d7223f3597bd3
Humberto Angelucci <angeluccirincon@gmail.com>
2021-06-30 08:57:15 +0200

    lab Done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,3 +1,5 @@
+//const { populate } = require("../models/Celebrity.model");
+
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
@@ -37,6 +39,63 @@ router.get('/movies', (req, res, next) => {
         .catch((err) => {
             next(err)
         })
+})
+
+router.get('/movies/:id', (req,res,next) => {
+    let id = req.params.id
+
+    MovieModel.findById(id)
+        .populate('cast')
+        .then((moviesDetail) => {
+            res.render('movies/movie-details.hbs', {moviesDetail})
+        })
+        .catch((err) => {
+            next(err)
+        })
+})
+
+router.post('/movies/:id/delete', (req, res, next) => {
+    let id = req.params.id
+
+    MovieModel.findByIdAndRemove(id)
+        .then(() => {
+            res.redirect('/movies');
+        })
+        .catch((err) => {
+            next(err);
+        })
+})
+
+router.get('/movies/:id/edit', (req,res,next) => {
+    let id = req.params.id
+
+    MovieModel.findById(id)
+        .then((selectecMovie) => {
+           
+            CeleModel.find({})
+            .then((cele) => {
+                res.render('movies/edit-movie.hbs', {cele, selectecMovie } )
+            })
+            .catch((err) => {
+                next(err)
+            })
+        })
+        .catch((err) => {
+            next(err)
+        })
+})
+
+router.post('/movies/:id/edit', (req,res,next) => {
+    let id = req.params.id;
+    const {title, genre, plot, cast} = req.body;
+
+    MovieModel.findByIdAndUpdate(id, {title, genre, plot, cast})
+    .then(() => {
+        res.redirect('/movies', id)
+    })
+    .catch((err) => {
+        next(err)
+    })
 })
 
 module.exports = router;
